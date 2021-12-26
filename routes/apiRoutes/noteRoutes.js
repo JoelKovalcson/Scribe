e4ade024f764d47886166d797dd2aa6568727e90
Joel Kovalcson <joelkovalcson@gmail.com>
2021-12-26 10:23:59 -0800

    Setup Express and added route skeleton.

---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const fs = require('fs');
+
+const notes = require('../../db/db.json');
+
+router.get('/notes', (req, res) => {
+    let results = notes;
+    console.log(results);
+});
+
+router.post('/notes', (req, res) => {
+    const note = req.body;
+    console.log(note);
+});
+
+router.delete('/notes/:id', (req, res) => {
+    let results = notes[req.params.id];
+    console.log(results);
+});
