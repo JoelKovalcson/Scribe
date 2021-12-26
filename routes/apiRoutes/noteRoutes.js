32ddc86fe3e50beb0bfc591a2c69bb9b65d9bb10
Joel Kovalcson <joelkovalcson@gmail.com>
2021-12-26 10:42:31 -0800

    get all notes endpoint now working

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -5,7 +5,7 @@ const notes = require('../../db/db.json');
 
 router.get('/notes', (req, res) => {
     let results = notes;
-    console.log(results);
+    res.json(results);
 });
 
 router.post('/notes', (req, res) => {
